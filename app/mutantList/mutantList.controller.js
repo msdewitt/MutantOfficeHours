27763ce45210d901e067b53ce5322f835893df3b
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-21 10:22:22 -0400

    Add send text object to firebase server

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -9,11 +9,27 @@ MutantListController.$inject =['$firebaseArray'];
     function MutantListController($firebaseArray){
         var vm = this;
         var mutantsRef = firebase.database().ref().child('mutants');
+        var textRef = firebase.database().ref().child('texts');
+
         vm.mutants =$firebaseArray(mutantsRef);
         vm.newMutant = new Mutant();
         vm.addMutant = addMutant;
         vm.toggleComplete = toggleComplete;
         vm.deleteMutant = deleteMutant;
+        vm.sendText = sendText;
+
+        function sendText(mutant){
+
+          var newText = {
+            topic: mutant.topic,
+            name: mutant.name,
+            phone: mutant.phone
+          };
+          textRef.push(newText);
+          mutant.notified = true;
+          vm.mutants.$save(mutant);
+        }
+
         function deleteMutant(mutant){
           vm.mutants.$remove(mutant);
         }
