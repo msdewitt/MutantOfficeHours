b7ee4014fa411c49fcbe20d9b176075e884dc566
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-23 10:35:58 -0400

    Add textMessage.service.js

---
+++ app/core/textMessage.service.js
@@ -0,0 +1,15 @@
+(function(){
+
+'use strict';
+
+angular
+  .module('mutantApp.core')
+  .service('textMessageService', textMessageService);
+
+  function textMessageService(){
+    var service = {}
+
+    return service;
+  }
+
+})();
