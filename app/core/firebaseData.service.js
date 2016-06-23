a579d2c86c7da3f0e1b4feb6ce3bc9ecef3d2997
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-23 09:46:31 -0400

    Add fireBaseData.service.js and add script tag on index.html.

---
+++ app/core/firebaseData.service.js
@@ -0,0 +1,19 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('mutantApp.core')
+    .factory('fireBaseDataService', fireBaseDataService);
+
+      function fireBaseDataService(){
+        var service = {}
+
+        return service;
+
+        //////////////////
+
+        
+      }
+
+
+})();
