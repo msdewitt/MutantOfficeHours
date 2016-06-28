7dd368dec2a5e0428df58eec9132ca44ff3c2169
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-28 11:07:54 -0400

    Moved deleteMutant and sendText into the mlMutantTable directive.

--- app/mutantList/directives/mutantTable.directive.js
+++ app/mutantList/directives/mutantTable.directive.js
@@ -9,7 +9,28 @@
       return{
         templatUrl: 'app/mutantList/directives/mutantTable.html',
         restrict: 'E',
+        controller: MutantTableController,
+        controllerAs: 'vm',
+        bindToController: true,
+        scope:{
+          mutants: '='
+        },
 
+      }
+    }
+    MutantTableController.$inject = ['textMessageService'];
+    function MutantTableController(textMessageService){
+      var vm = this;
+
+      vm.deleteMutant = deleteMutant;
+      vm.sendText = sendText;
+
+
+      function deleteMutant(mutant) {
+        vm.mutants.$remove(mutant);
+      }
+      function sendText(mutant) {
+        textMessageService.sendText(mutant, vm.mutants);
       }
     }
 })();
